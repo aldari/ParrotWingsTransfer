2d342bb1063d685933c2a6cde503c07c20bdb253
El'dar Ismagilov <eldarismagilov@gmail.com>
2016-12-04 19:33:46 +0500

    AngularJS Token Authentication using ASP.NET Web API 2, Owin, and Identity

---
+++ ParrotWingsTransfer.Web/app/services/ordersService.js
@@ -0,0 +1,18 @@
+'use strict';
+app.factory('ordersService', ['$http', function ($http) {
+ 
+    var serviceBase = 'http://localhost:55452/';
+    var ordersServiceFactory = {};
+ 
+    var _getOrders = function () {
+ 
+        return $http.get(serviceBase + 'api/orders').then(function (results) {
+            return results;
+        });
+    };
+ 
+    ordersServiceFactory.getOrders = _getOrders;
+ 
+    return ordersServiceFactory;
+ 
+}]);
